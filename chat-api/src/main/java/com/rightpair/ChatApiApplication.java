aebc905600ab887402740c7d643c1bd49850f4d4
rightpair <tkddn204@gmail.com>
2023-12-05 11:38:05 +0900

    :tada: Initialize Commit

---
+++ chat-api/src/main/java/com/rightpair/ChatApiApplication.java
@@ -0,0 +1,11 @@
+package com.rightpair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApiApplication.class, args);
+    }
+}
---
+++ chat-auth/src/main/java/com/rightpair/ChatAuthApplication.java
@@ -0,0 +1,11 @@
+package com.rightpair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAuthApplication.class, args);
+    }
+}
