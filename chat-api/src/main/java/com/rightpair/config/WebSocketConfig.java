0f2500c396a2700dad0241a23c16f4f10d3aab62
rightpair <tkddn204@gmail.com>
2023-12-07 12:04:22 +0900

    :sparkles: WebSocket 설정 추가
    Signed-off-by: rightpair <tkddn204@gmail.com>

---
+++ chat-api/src/main/java/com/rightpair/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.rightpair.config;
+
+import com.rightpair.websocket.ChatWebSocketHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@RequiredArgsConstructor
+@EnableWebSocket
+@Configuration
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final ChatWebSocketHandler chatWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatWebSocketHandler, "/chat").setAllowedOriginPatterns("/**");
+    }
+}
